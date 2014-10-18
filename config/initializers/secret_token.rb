4b52892a569c45e9a103f93bad2e5c2c99511654
Nicolas Gamblin <nicolas.gamblin@live.fr>
2014-10-18 14:53:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cerebrawl::Application.config.secret_token = '9b5bb9605548b35974f11338c5dd414470d0340cf0b386770b47d018e156a2cbe9bfb86ad82c455d48b2d89571a545f9bfedb40eeea0961c2e4d61e603a7adc2'
